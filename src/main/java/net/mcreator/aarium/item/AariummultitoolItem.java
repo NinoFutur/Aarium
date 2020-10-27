ee72b8376de661320233897a6b82f48448ab0ac6
NinoFutur <nino_@LAPTOP-L57G0M0C.home>
2020-10-27 10:51:29 +0100

    added some new paints and one tool

---
+++ src/main/java/net/mcreator/aarium/item/AariummultitoolItem.java
@@ -0,0 +1,78 @@
+
+package net.mcreator.aarium.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.aarium.AariumModElements;
+
+import com.google.common.collect.Multimap;
+
+@AariumModElements.ModElement.Tag
+public class AariummultitoolItem extends AariumModElements.ModElement {
+	@ObjectHolder("aarium:aariummultitool")
+	public static final Item block = null;
+	public AariummultitoolItem(AariumModElements instance) {
+		super(instance, 142);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemToolCustom() {
+		}.setRegistryName("aariummultitool"));
+	}
+	private static class ItemToolCustom extends Item {
+		protected ItemToolCustom() {
+			super(new Item.Properties().group(ItemGroup.TOOLS).maxDamage(8000));
+		}
+
+		@Override
+		public Multimap<String, AttributeModifier> getAttributeModifiers(EquipmentSlotType equipmentSlot) {
+			Multimap<String, AttributeModifier> multimap = super.getAttributeModifiers(equipmentSlot);
+			if (equipmentSlot == EquipmentSlotType.MAINHAND) {
+				multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(),
+						new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Tool modifier", 5f, AttributeModifier.Operation.ADDITION));
+				multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(),
+						new AttributeModifier(ATTACK_SPEED_MODIFIER, "Tool modifier", -3, AttributeModifier.Operation.ADDITION));
+			}
+			return multimap;
+		}
+
+		@Override
+		public boolean canHarvestBlock(BlockState state) {
+			return 5 >= state.getHarvestLevel();
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack itemstack, BlockState blockstate) {
+			return 24f;
+		}
+
+		@Override
+		public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+			stack.damageItem(1, attacker, i -> i.sendBreakAnimation(EquipmentSlotType.MAINHAND));
+			return true;
+		}
+
+		@Override
+		public boolean onBlockDestroyed(ItemStack stack, World worldIn, BlockState state, BlockPos pos, LivingEntity entityLiving) {
+			stack.damageItem(1, entityLiving, i -> i.sendBreakAnimation(EquipmentSlotType.MAINHAND));
+			return true;
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 26;
+		}
+	}
+}
