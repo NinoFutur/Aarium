7005d42da83ab9543146c7cd19aed9625094e98e
NinoFutur <nino_@LAPTOP-L57G0M0C.home>
2020-10-28 16:48:22 +0100

    Added Volcanite Apple

---
+++ src/main/java/net/mcreator/aarium/procedures/VolcaniteappleFoodEatenProcedure.java
@@ -0,0 +1,30 @@
+package net.mcreator.aarium.procedures;
+
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.aarium.AariumModElements;
+
+import java.util.Map;
+
+@AariumModElements.ModElement.Tag
+public class VolcaniteappleFoodEatenProcedure extends AariumModElements.ModElement {
+	public VolcaniteappleFoodEatenProcedure(AariumModElements instance) {
+		super(instance, 145);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				System.err.println("Failed to load dependency entity for procedure VolcaniteappleFoodEaten!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.FIRE_RESISTANCE,
+					(int) ((500 + 20) * ((entity instanceof PlayerEntity) ? ((PlayerEntity) entity).experienceLevel : 0)), (int) 5));
+	}
+}
